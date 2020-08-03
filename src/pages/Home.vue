3fcd737442e958a73fe485f6316fc3cbd069381e
Mishael Nuh <mishael.ebel.nuh@gmail.com>
2020-08-03 04:13:46 -0400

    Add tags to pages.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <b-row no-gutters style="position: fixed" id="homeHeader" v-show="showHeader">
+    <b-row no-gutters id="homeHeader" v-show="showHeader">
       <b-col xs="10" sm="8" align-self="center">
-        <div id="homeHeaderContent">
+        <div id="homeHeaderContent" ref="homeHeader">
           <h1>mishael nuh</h1>
           <p>
             Hello there! I am an Engineering Science student majoring in Infrastructure Engineering at the University of Toronto interested in the interplay between digital technologies and structural engineering. This website is aimed at showcasing some of my work. Some of them are serious and others not so much.
@@ -27,21 +27,33 @@
     </b-row>
     <b-row no-gutters id="homeHeaderSpacer">
     </b-row>
-    <b-row class="py-3 pl-5" ref="portfolioHeader" style="background-color: white;">
+    <b-row class="sticky-top" ref="portfolioHeader" id="portfolioHeader">
       <b-col>
-        <h1>portfolio</h1>
+        <h2>portfolio</h2>
+        <div>
+          <b-badge :variant="filteredTags.includes(t) ? 'highlight' : 'light'" v-for="t in tags" :key="t" class="mr-2 mb-2" @click="setTag(t)">{{t}}</b-badge>
+        </div>
       </b-col>
     </b-row>
-    <b-row no-gutters>
-      <b-col :md="p.width" v-for="p in filteredPages" :key="p.id">
-          <b-card class="hoverCard" style="height: 100%;" img-bottom :img-src="p.image" @click="clickPage(p)">
-            <b-card-body style="text-align: left;">
-              <b-card-text>
-                <h3>{{p.title}}</h3>
-                {{p.shortDesc}}
-              </b-card-text>
-            </b-card-body>
-          </b-card>
+    <b-row no-gutters style="background-color: white; min-height: 100vh;">
+      <b-col>
+        <b-row no-gutters>
+          <b-col :md="p.width" v-for="p in filteredPages" :key="p.id">
+              <b-card class="hoverCard" style="height: 100%;" img-top :img-src="p.image" @click="clickPage(p)">
+                <b-card-body style="text-align: left;">
+                  <b-card-text>
+                    <h3>{{p.title}}</h3>
+                    <div v-if="p.tags">
+                      <b-badge :variant="filteredTags.includes(t) ? 'highlight' : 'light'" v-for="t in p.tags.sort()" :key="t" class="mr-2 mb-2">{{t}}</b-badge>
+                    </div>
+                    <br/>
+                    {{p.shortDesc}}
+                  </b-card-text>
+                  
+                </b-card-body>
+              </b-card>
+          </b-col>
+        </b-row>
       </b-col>
     </b-row>
   </div>
@@ -54,18 +66,27 @@ export default {
   name: "Home",
   data() {
     return {
+      scrollThreshold: 0,
       pages: pageData,
       showHeader: true,
+      filteredTags: []
     };
   },
   computed: {
     filteredPages() {
       var filteredPages = JSON.parse(JSON.stringify(this.pages))
-
+      
+      if (this.filteredTags.length > 0) {
+        console.log(this.filteredTags)
+        filteredPages = filteredPages.filter(p => 
+          this.filteredTags.every(t => p.tags.includes(t))
+        )
+      }
+      
       var currAllowance = 12
-      const widthRange = [3, 5]
+      const widthRange = [3, 6]
 
-      filteredPages.forEach((p, ) => {
+      filteredPages.forEach(p => {
         p.width = Math.floor(Math.random()*(widthRange[1] - widthRange[0])) + widthRange[0]
         if (currAllowance - p.width < widthRange[0]) {
           p.width = currAllowance
@@ -76,10 +97,19 @@ export default {
       })
 
       return filteredPages
+    },
+    tags() {
+      var tags = []
+      this.pages.forEach(p => {
+        tags.push(...p.tags)
+      })
+      var uniqueTags = [...new Set(tags)]
+      return uniqueTags.sort()
     }
   },
   created () {
     window.addEventListener('scroll', this.handleScroll);
+    this.setCardWidths();
   },
   destroyed() {
     window.removeEventListener('scroll', this.handleScroll);
@@ -90,10 +120,23 @@ export default {
     clickPage(page) {
       this.$router.push('/page/' + page.id)
     },
-    handleScroll () {
+    handleScroll() {
+      const homeHeaderTop = this.$refs.homeHeader.getBoundingClientRect().top
       const portfolioTop = this.$refs.portfolioHeader.getBoundingClientRect().top
 
-      this.showHeader = portfolioTop > 0
+      if (!this.showHeader) {
+        this.showHeader = this.scrollThreshold < portfolioTop
+      } else {
+        this.showHeader = homeHeaderTop < portfolioTop
+        this.scrollThreshold = homeHeaderTop
+      }
+    },
+    setTag(tag) {
+      if (this.filteredTags.includes(tag)) {
+        this.filteredTags.splice(this.filteredTags.indexOf(tag), 1)
+      } else {
+        this.filteredTags.push(tag)
+      }
     }
   }
 };
